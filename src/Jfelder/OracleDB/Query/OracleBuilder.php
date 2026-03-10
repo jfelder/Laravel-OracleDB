96a1e56cabfa77bf6c53fb203a1773ba0b25151c
Jimmy Felder <jimmyfelder@gmail.com>
2026-03-10 11:39:18 -0400

    fix(query): support subqueries in whereBetween

--- src/Jfelder/OracleDB/Query/OracleBuilder.php
+++ src/Jfelder/OracleDB/Query/OracleBuilder.php
@@ -2,6 +2,7 @@
 
 namespace Jfelder\OracleDB\Query;
 
+use Carbon\CarbonPeriod;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Query\Expression;
 
@@ -25,5 +26,30 @@ class OracleBuilder extends Builder
         $this->joins[] = $this->newJoinClause($this, 'cross', new Expression($expression));
 
         return $this;
+    }
+
+    /**
+     * Add a where between statement to the query.
+     *
+     * @param  \Illuminate\Contracts\Database\Query\Expression|string  $column
+     * @param  string  $boolean
+     * @param  bool  $not
+     * @return $this
+     */
+    public function whereBetween($column, iterable $values, $boolean = 'and', $not = false)
+    {
+        if ($values instanceof CarbonPeriod) {
+            $values = [$values->getStartDate(), $values->getEndDate()];
+        }
+
+        if ($this->isQueryable($column)) {
+            [$query, $bindings] = $this->createSub($column);
+
+            $column = new Expression("({$query})");
+
+            $this->addBinding($bindings, 'where');
+        }
+
+        return parent::whereBetween($column, $values, $boolean, $not);
     }
 }
